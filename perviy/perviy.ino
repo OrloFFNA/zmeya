644d938e7fbd4fe825292f1838ed27083310d0cf
NikitaOrlov357 <72908590+NikitaOrlov357@users.noreply.github.com>
2021-05-27 20:05:26 +0300

    поправить условия работы границ змейки

--- perviy/perviy.ino
+++ perviy/perviy.ino
@@ -1,27 +1,78 @@
 #include <TroykaLedMatrix.h>
-#define Y A3
-#define X A2
-#define Z 3
+#define Yj A3
+#define Xj A2
+#define Zj 3
 TroykaLedMatrix matrix;
 
+int X, Y;
+
+void setupSnake() {
+  randomSeed(analogRead(0));
+  X = random(8);
+  Y = random(8);
+  matrix.drawPixel(X, Y);
+}
+
+void right() {
+  if(X < 8){
+    matrix.clearPixel(X, Y);
+    X++;
+    matrix.drawPixel(X, Y);
+  }
+}
+
+void left() {
+  if(X < 8){
+    matrix.clearPixel(X, Y);
+    X--;
+    matrix.drawPixel(X, Y);
+  }
+}
+
+void down() {
+  if(X < 8){
+    matrix.clearPixel(X, Y);
+    Y++;
+    matrix.drawPixel(X, Y);
+  }
+}
+
+void up() {
+  if(X < 8){
+    matrix.clearPixel(X, Y);
+    Y--;
+    matrix.drawPixel(X, Y);
+  }
+}
 void setup() {
   matrix.begin();
   Serial.begin(9600);
+  setupSnake();
   
 }
 void loop() {
 int x,y,z;
-z = digitalRead(Z);
-x = analogRead(X);
-y = analogRead(Y);
-
+z = digitalRead(Zj);
+x = analogRead(Xj);
+y = analogRead(Yj);
+if(x > 600){
+  right();
+}
+if(x < 400){
+  left();
+}
+if(y > 600){
+  up();
+}
+if(y < 400){
+  down();
+}
+delay(100);
   /*for(int y = 0; y < 8; y++){
     for(int x = 0; x < 8; x++){
       matrix.drawPixel(x, y);
       delay(300);
-     
     }
-   
   }
   matrix.clear();
 */
