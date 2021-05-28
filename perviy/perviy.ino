08b43c75c426a70e252f8c3133dbe5a8d214052f
NikitaOrlov357 <72908590+NikitaOrlov357@users.noreply.github.com>
2021-05-28 14:11:07 +0300

    добавлен список для хранения координат змейки

--- perviy/perviy.ino
+++ perviy/perviy.ino
@@ -2,7 +2,81 @@
 #define Yj A3
 #define Xj A2
 #define Zj 3
+
+
+
+class List{
+  class Node{
+    public:
+    int x;
+    int y;
+    Node* next;
+    Node(int x, int y){
+      this->x = x;
+      this->y = y;
+      next = NULL;   
+    }
+  };
+
+  public:
+  Node* head;
+
+  List(){
+    head = NULL;
+  }
+
+  void push_back(int x, int y){
+    if (head == NULL){
+      head = new Node(x, y);
+    }
+    else{
+      Node* current = head;
+      while (current->next){
+        current = current->next;
+      }
+      current->next = new Node(x, y);
+    }
+  }
+
+   void print(){
+    Node* current = head;
+      while (current){
+        Serial.print(current->x);
+        Serial.print( " " );
+        Serial.println(current->y);
+        current = current->next;
+        
+      }
+  }
+
+  void pop(){
+    Node* current = head;
+      while (current->next->next){
+        current = current->next;
+      }
+      current->next = NULL;
+  }
+
+  void push_start(int x, int y){
+    Node* n = new Node(x, y);
+    n->next = head;
+    head = n;
+  }
+
+  int getHeadX(){
+    return head->x;
+  }
+
+  int getHeadY(){
+    return head->y;
+  }
+
+};
+
+
 TroykaLedMatrix matrix;
+
+
 
 int X, Y;
 
@@ -13,7 +87,7 @@ void setupSnake() {
   Y = random(8);
   matrix.drawPixel(0, 0);
 }
-
+ 
 void drawBitMap(int bitMap[8][8]){
   for(int y = 0; y < 8; y++){
     for(int x = 0; x < 8; x++){
@@ -79,8 +153,13 @@ void setup() {
   setupSnake();
   
 }
-void loop() {
-  int bitMap[8][8] = {
+
+List a;
+
+void loop() { 
+ a.push_back(3,5);
+ a.print();
+ /* int bitMap[8][8] = {
     {0,0,0,0,0,0,0,0},
     {0,1,1,0,0,1,1,0},
     {0,1,1,0,0,1,1,0},
@@ -92,7 +171,8 @@ void loop() {
   };
   drawBitMap(bitMap);
   delay(1000);
-/*int x,y,z;
+
+int x,y,z;
 z = digitalRead(Zj);
 x = analogRead(Xj);
 y = analogRead(Yj);
@@ -102,16 +182,16 @@ Serial.println(x);
 Serial.print("y = ");
 Serial.println(y);
 
-if(x > 900){
+if(x > 700){
   right();
 }
-if(x < 100){
+if(x < 300){
   left();
 }
-if(y > 900){
+if(y > 700){
   up();
 }
-if(y < 100){
+if(y < 300){
   down();
 }
 delay(100);
